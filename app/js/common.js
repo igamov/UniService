a4cd18429d651077c915fad0d132cbc6183cb46a
Dilyara <Gabdreeva_Dilya@mail.ru>
2018-03-25 13:15:28 +0300

    pages are responsive for different sizes

---
+++ app/js/common.js
@@ -0,0 +1,43 @@
+$(function( ) {
+    var $menu = $("#menu").mmenu({
+            "extensions": {
+                "all": [
+                    "pagedim-black",
+                    "position-right",
+                    "theme-white"],
+                "(max-width: 400px)": ["fullscreen"],
+                "(min-height: 768px)": ["listview-large"]
+            },
+            "navbars": [
+                {
+                    "position": "top"
+                }
+            ],
+            "navbar" : {
+                "title": "UniService"
+            }
+        },
+        {
+            // configuration
+            clone: true
+        }
+    );
+
+    var $icon = $("#btn-menu");
+    var API = $menu.data( "mmenu" );
+    $icon.on( "click", function() {
+        API.open();
+    });
+    API.bind( "open:finish", function() {
+        setTimeout(function() {
+            $icon.addClass( "is-active" );
+        }, 100);
+    });
+    API.bind( "close:finish", function() {
+        setTimeout(function () {
+            $icon.removeClass("is-active");
+        }, 100);
+    });
+
+});
+
